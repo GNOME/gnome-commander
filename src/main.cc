0670acb198074c3dc420ccbfca9ce60b2f15fbd3
Piotr Eljasiak <epiotr@src.gnome.org>
2007-10-27 22:32:12 +0000

    Call gnome_vfs_init() & gnome_authentication_manager_init() in proper order
    svn path=/trunk/; revision=1248

--- src/main.cc
+++ src/main.cc
@@ -111,11 +111,11 @@ int main (int argc, char *argv[])
                                   GNOME_PARAM_APP_DATADIR, DATADIR,
                                   GNOME_PARAM_NONE);
 
-    gnome_authentication_manager_init ();
-
     ls_colors_init ();
     gdk_rgb_init ();
     gnome_vfs_init ();
+    gnome_authentication_manager_init ();
+
     conf_dir = g_build_path (G_DIR_SEPARATOR_S, g_get_home_dir(), ".gnome-commander", NULL);
     create_dir_if_needed (conf_dir);
     g_free (conf_dir);
