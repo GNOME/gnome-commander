ac67f3aed276ec1101beb51f6bb946624f3f78ad
Piotr Eljasiak <epiotr@src.gnome.org>
2010-06-03 20:50:53 +0200

    vte: properly unregister CTRL+O when used for options.edit

--- src/gnome-cmd-user-actions.cc
+++ src/gnome-cmd-user-actions.cc
@@ -351,11 +351,8 @@ void GnomeCmdUserActions::init()
     if (!registered("view.close_tab"))
         register_action(GDK_CONTROL_MASK, GDK_W, "view.close_tab");
 
-    if (!registered("view.terminal"))
-    {
-        unregister(GDK_CONTROL_MASK, GDK_O);                       // unregister CTRL+O as it was used previously for options.edit
-        register_action(GDK_CONTROL_MASK, GDK_O, "view.terminal");
-    }
+    unregister(GDK_CONTROL_MASK, GDK_O);
+    register_action(GDK_CONTROL_MASK | GDK_SHIFT_MASK, GDK_O, "options.edit");
 
     unregister(GDK_F9);                                 // unregister F9 if defined in [key-bindings]
     register_action(GDK_F9, "edit.search");             // and overwrite it with edit.search action
