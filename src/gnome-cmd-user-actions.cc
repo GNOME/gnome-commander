8b94c1f5c6d475725f1e7678934ed2e557899cc0
Piotr Eljasiak <epiotr@src.gnome.org>
2010-06-03 19:47:18 +0200

    Removed surplus diagnostics

--- src/gnome-cmd-user-actions.cc
+++ src/gnome-cmd-user-actions.cc
@@ -899,7 +899,6 @@ void file_diff (GtkMenuItem *menuitem, gpointer not_used)
     {
         gchar *cmd = g_strdup_printf (gnome_cmd_data.get_differ(), s.c_str(), "");
 
-        g_print (_("running `%s'\n"), cmd);
         run_command (cmd);
 
         g_free (cmd);
@@ -925,7 +924,6 @@ void file_sync_dirs (GtkMenuItem *menuitem, gpointer not_used)
 
     gchar *cmd = g_strdup_printf (gnome_cmd_data.get_differ(), s.c_str(), "");
 
-    g_print (_("running `%s'\n"), cmd);
     run_command (cmd);
 
     g_free (cmd);
