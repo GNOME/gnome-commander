GNOME/gnome-commander

commit 8dfe896d52cd9b94718b55f90f326123454b69c0

GnomeCmdDir: added gnome_cmd_dir_is_monitored()

src/gnome-cmd-dir.cc
@@ -863,6 +863,14 @@ void gnome_cmd_dir_cancel_monitoring (GnomeCmdDir *dir)
 }
 
 
+void gnome_cmd_dir_is_monitored (GnomeCmdDir *dir)
+{
+    g_return_val_if_fail (GNOME_CMD_IS_DIR (dir), FALSE);
+
+    return dir->priv->monitor_users > 0;
+}
+
+
 gboolean gnome_cmd_dir_is_local (GnomeCmdDir *dir)
 {
     g_return_val_if_fail (GNOME_CMD_IS_DIR (dir), FALSE);

src/gnome-cmd-dir.h
@@ -146,6 +146,7 @@ gboolean gnome_cmd_dir_uses_fam (GnomeCmdDir *dir);
 
 void gnome_cmd_dir_start_monitoring (GnomeCmdDir *dir);
 void gnome_cmd_dir_cancel_monitoring (GnomeCmdDir *dir);
+void gnome_cmd_dir_is_monitored (GnomeCmdDir *dir);
 gboolean gnome_cmd_dir_is_local (GnomeCmdDir *dir);
 void gnome_cmd_dir_set_content_changed (GnomeCmdDir *dir);
 

commit fa43d679e91146188f445cd19583f886c6c072fa

Fix for bug introduced in 8dfe896d52cd9b94718b55f90f326123454b69c0 commit

src/gnome-cmd-dir.cc
@@ -863,7 +863,7 @@ void gnome_cmd_dir_cancel_monitoring (GnomeCmdDir *dir)
 }
 
 
-void gnome_cmd_dir_is_monitored (GnomeCmdDir *dir)
+gboolean gnome_cmd_dir_is_monitored (GnomeCmdDir *dir)
 {
     g_return_val_if_fail (GNOME_CMD_IS_DIR (dir), FALSE);
 

src/gnome-cmd-dir.h
@@ -146,7 +146,7 @@ gboolean gnome_cmd_dir_uses_fam (GnomeCmdDir *dir);
 
 void gnome_cmd_dir_start_monitoring (GnomeCmdDir *dir);
 void gnome_cmd_dir_cancel_monitoring (GnomeCmdDir *dir);
-void gnome_cmd_dir_is_monitored (GnomeCmdDir *dir);
+gboolean gnome_cmd_dir_is_monitored (GnomeCmdDir *dir);
 gboolean gnome_cmd_dir_is_local (GnomeCmdDir *dir);
 void gnome_cmd_dir_set_content_changed (GnomeCmdDir *dir);
 