GNOME/gnome-commander

commit ad68b01d5a19a33cfbc97f222e3deba6cfcab152

Fixed problem #638954 (search path is wrong for devices)

src/gnome-cmd-search-dialog.cc
@@ -974,8 +974,7 @@ GtkWidget *gnome_cmd_search_dialog_new (GnomeCmdDir *default_dir)
     gchar *new_path;
     GnomeCmdSearchDialog *dialog = (GnomeCmdSearchDialog *) gtk_type_new (gnome_cmd_search_dialog_get_type ());
 
-    gchar *path = gnome_cmd_file_get_path (GNOME_CMD_FILE (default_dir));
-    if (path[strlen(path)-1] != '/')
+    gchar *path = gnome_cmd_dir_is_local (default_dir) ? gnome_cmd_file_get_real_path (GNOME_CMD_FILE (default_dir)) : gnome_cmd_file_get_path (GNOME_CMD_FILE (default_dir));
     {
         new_path = g_strdup_printf ("%s/", path);
         g_free (path);

doc/C/gnome-commander.xml
@@ -6077,6 +6077,9 @@
                             <para>Fixed problem #637501 (advrename: metatag popup menu shows wrong items)</para>
                         </listitem>
                         <listitem>
+                            <para>Fixed problem #638954 (search path is wrong for devices)</para>
+                        </listitem>
+                        <listitem>
                             <para>Fixed problem with toggling path/basename/filename selections in copy/move dialogs</para>
                         </listitem>
                         <listitem>

commit b9750b8dc05aae987557decd711b90834cd8b7a5

Fix for bug introduced in commit ad68b01d5a19a33cfbc97f222e3deba6cfcab152 (fixed problem #638954 ...)

src/gnome-cmd-search-dialog.cc
@@ -975,6 +975,8 @@ GtkWidget *gnome_cmd_search_dialog_new (GnomeCmdDir *default_dir)
     GnomeCmdSearchDialog *dialog = (GnomeCmdSearchDialog *) gtk_type_new (gnome_cmd_search_dialog_get_type ());
 
     gchar *path = gnome_cmd_dir_is_local (default_dir) ? gnome_cmd_file_get_real_path (GNOME_CMD_FILE (default_dir)) : gnome_cmd_file_get_path (GNOME_CMD_FILE (default_dir));
+
+    if (path[strlen(path)-1] != '/')
     {
         new_path = g_strdup_printf ("%s/", path);
         g_free (path);