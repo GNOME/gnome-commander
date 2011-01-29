d6ecd7ae6349080c969a3441ab4c01846ed5953d
Piotr Eljasiak <epiotr@src.gnome.org>
2011-01-29 23:05:01 +0100

    noop: code cleanup (gnome_cmd_search_dialog_get_type() -> GNOME_CMD_TYPE_SEARCH_DIALOG)

--- src/gnome-cmd-search-dialog.h
+++ src/gnome-cmd-search-dialog.h
@@ -23,12 +23,12 @@
 
 #include "gnome-cmd-dir.h"
 
-#define GNOME_CMD_SEARCH_DIALOG(obj) \
-    GTK_CHECK_CAST (obj, gnome_cmd_search_dialog_get_type (), GnomeCmdSearchDialog)
-#define GNOME_CMD_SEARCH_DIALOG_CLASS(klass) \
-    GTK_CHECK_CLASS_CAST (klass, gnome_cmd_search_dialog_get_type (), GnomeCmdSearchDialogClass)
-#define GNOME_CMD_IS_SEARCH_DIALOG(obj) \
-    GTK_CHECK_TYPE (obj, gnome_cmd_search_dialog_get_type ())
+#define GNOME_CMD_TYPE_SEARCH_DIALOG              (gnome_cmd_search_dialog_get_type ())
+#define GNOME_CMD_SEARCH_DIALOG(obj)              (G_TYPE_CHECK_INSTANCE_CAST((obj), GNOME_CMD_TYPE_SEARCH_DIALOG, GnomeCmdSearchDialog))
+#define GNOME_CMD_SEARCH_DIALOG_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST((klass), GNOME_CMD_TYPE_SEARCH_DIALOG, GnomeCmdSearchDialogClass))
+#define GNOME_CMD_IS_SEARCH_DIALOG(obj)           (G_TYPE_CHECK_INSTANCE_TYPE((obj), GNOME_CMD_TYPE_SEARCH_DIALOG))
+#define GNOME_CMD_IS_SEARCH_DIALOG_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE ((klass), GNOME_CMD_TYPE_SEARCH_DIALOG))
+#define GNOME_CMD_SEARCH_DIALOG_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS((obj), GNOME_CMD_TYPE_SEARCH_DIALOG, GnomeCmdSearchDialogClass))
 
 
 struct GnomeCmdSearchDialogPrivate;
