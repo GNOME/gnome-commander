c552ce7eac841e44403346879e8e5a177a635716
Piotr Eljasiak <epiotr@src.gnome.org>
2011-01-29 23:13:23 +0100

    noop: code cleanup (gnome_cmd_chmod_component_get_type() -> GNOME_CMD_TYPE_CHMOD_COMPONENT)

--- src/gnome-cmd-chmod-component.h
+++ src/gnome-cmd-chmod-component.h
@@ -23,9 +23,12 @@
 
 #include "gnome-cmd-file.h"
 
-#define GNOME_CMD_TYPE_CHMOD_COMPONENT      (gnome_cmd_chmod_component_get_type ())
-#define GNOME_CMD_CHMOD_COMPONENT(obj)      (GTK_CHECK_CAST (obj, GNOME_CMD_TYPE_CHMOD_COMPONENT, GnomeCmdChmodComponent))
-#define GNOME_CMD_IS_CHMOD_COMPONENT(obj)   (GTK_CHECK_TYPE (obj, GNOME_CMD_TYPE_CHMOD_COMPONENT))
+#define GNOME_CMD_TYPE_CHMOD_COMPONENT              (gnome_cmd_chmod_component_get_type ())
+#define GNOME_CMD_CHMOD_COMPONENT(obj)              (G_TYPE_CHECK_INSTANCE_CAST((obj), GNOME_CMD_TYPE_CHMOD_COMPONENT, GnomeCmdChmodComponent))
+#define GNOME_CMD_CHMOD_COMPONENT_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST((klass), GNOME_CMD_TYPE_CHMOD_COMPONENT, GnomeCmdChmodComponentClass))
+#define GNOME_CMD_IS_CHMOD_COMPONENT(obj)           (G_TYPE_CHECK_INSTANCE_TYPE((obj), GNOME_CMD_TYPE_CHMOD_COMPONENT))
+#define GNOME_CMD_IS_CHMOD_COMPONENT_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE ((klass), GNOME_CMD_TYPE_CHMOD_COMPONENT))
+#define GNOME_CMD_CHMOD_COMPONENT_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS((obj), GNOME_CMD_TYPE_CHMOD_COMPONENT, GnomeCmdChmodComponentClass))
 
 
 struct GnomeCmdChmodComponentPrivate;
