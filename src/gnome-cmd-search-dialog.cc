GNOME/gnome-commander

commit 72cacbac293e866310b608d57f4ff08a61a4410b

search: ref found GnomeCmdFile

src/gnome-cmd-search-dialog.cc
@@ -324,7 +324,7 @@ static void search_dir_r (GnomeCmdDir *dir, SearchData *data)
 
                 // the file matched the search criteria, lets add it to the list
                 g_mutex_lock (data->pdata.mutex);
-                data->pdata.files = g_list_append (data->pdata.files, f);
+                data->pdata.files = g_list_append (data->pdata.files, f->ref());
                 g_mutex_unlock (data->pdata.mutex);
 
                 // also ref each directory that has a matching file
@@ -385,7 +385,7 @@ static gboolean update_search_status_widgets (SearchData *data)
 
     if (data->pdata.files)
     {
-        g_list_free (data->pdata.files);
+        gnome_cmd_file_list_free (data->pdata.files);
         data->pdata.files = NULL;
     }
 

commit 57124aa5661d8e78035daee54628acd2cb72067a

Fix for bug introduced in commit 72cacbac293e866310b608d57f4ff08a61a4410b

src/gnome-cmd-search-dialog.cc
@@ -324,7 +324,7 @@ static void search_dir_r (GnomeCmdDir *dir, SearchData *data)
 
                 // the file matched the search criteria, lets add it to the list
                 g_mutex_lock (data->pdata.mutex);
-                data->pdata.files = g_list_append (data->pdata.files, f->ref());
+                data->pdata.files = g_list_append (data->pdata.files, gnome_cmd_file_ref (f));
                 g_mutex_unlock (data->pdata.mutex);
 
                 // also ref each directory that has a matching file