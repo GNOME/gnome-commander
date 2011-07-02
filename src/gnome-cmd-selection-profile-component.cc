2cba190e3be4cee26e208fa1a319bb4af6f99d30
Piotr Eljasiak <epiotr@src.gnome.org>
2011-07-02 23:37:45 +0200

    GnomeCmdSelectionProfileComponent: store match_case in profile

--- src/gnome-cmd-selection-profile-component.cc
+++ src/gnome-cmd-selection-profile-component.cc
@@ -223,6 +223,7 @@ void GnomeCmdSelectionProfileComponent::copy()
     profile.max_depth = gtk_combo_box_get_active (GTK_COMBO_BOX (priv->recurse_combo)) - 1;
     stringify(profile.text_pattern, gtk_combo_box_get_active_text (GTK_COMBO_BOX (priv->find_text_combo)));
     profile.content_search = !profile.text_pattern.empty() && gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (priv->find_text_check));
+    profile.match_case = profile.content_search && gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (priv->case_check));
 }
 
 
@@ -233,6 +234,7 @@ void GnomeCmdSelectionProfileComponent::copy(GnomeCmdData::Selection &profile)
     profile.max_depth = gtk_combo_box_get_active (GTK_COMBO_BOX (priv->recurse_combo)) - 1;
     stringify(profile.text_pattern, gtk_combo_box_get_active_text (GTK_COMBO_BOX (priv->find_text_combo)));
     profile.content_search = !profile.text_pattern.empty() && gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (priv->find_text_check));
+    profile.match_case = profile.content_search && gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (priv->case_check));
 }
 
 
