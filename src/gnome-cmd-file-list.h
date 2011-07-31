01b01e03bac4c6f1a164856a3a668aab48f2c2c9
Piotr Eljasiak <epiotr@src.gnome.org>
2011-07-31 14:53:48 +0200

    Replacing obsoleted GtkType with GType

--- src/gnome-cmd-file-list.h
+++ src/gnome-cmd-file-list.h
@@ -36,7 +36,7 @@
 #define GNOME_CMD_FILE_LIST_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS((obj), GNOME_CMD_TYPE_FILE_LIST, GnomeCmdFileListClass))
 
 
-GtkType gnome_cmd_file_list_get_type ();
+GType gnome_cmd_file_list_get_type ();
 
 
 /* DnD target names */
