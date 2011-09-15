GNOME/gnome-commander

commit 314b149683bae365f579d559ec1163e9b33deecb

GnomeCmdStringDialog: drop typedef struct

libgcmd/gnome-cmd-string-dialog.cc
@@ -24,7 +24,7 @@
 #include "gnome-cmd-string-dialog.h"
 #include "libgcmd-widget-factory.h"
 
-struct _GnomeCmdStringDialogPrivate
+struct GnomeCmdStringDialogPrivate
 {
     GnomeCmdStringDialogCallback ok_cb;
     GFunc cancel_cb;

libgcmd/gnome-cmd-string-dialog.h
@@ -29,13 +29,7 @@
 #define GNOME_CMD_STRING_DIALOG_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS((obj), GNOME_CMD_TYPE_STRING_DIALOG, GnomeCmdStringDialogClass))
 
 
-typedef struct _GnomeCmdStringDialog GnomeCmdStringDialog;
-typedef struct _GnomeCmdStringDialogPrivate GnomeCmdStringDialogPrivate;
-typedef struct _GnomeCmdStringDialogClass GnomeCmdStringDialogClass;
-
-
-
-struct _GnomeCmdStringDialog
+xstruct GnomeCmdStringDialog
 {
     GnomeCmdDialog parent;
 
@@ -47,7 +41,7 @@ struct _GnomeCmdStringDialog
 };
 
 
-struct _GnomeCmdStringDialogClass
+struct GnomeCmdStringDialogClass
 {
     GnomeCmdDialogClass parent_class;
 };

commit 5392453f51347daa072dbec8fd7445f5132bb5eb

Fix for bug introduced in commit 314b149683bae365f579d559ec1163e9b33deecb

libgcmd/gnome-cmd-string-dialog.cc
@@ -24,7 +24,7 @@
 #include "gnome-cmd-string-dialog.h"
 #include "libgcmd-widget-factory.h"
 
-struct GnomeCmdStringDialogPrivate
+struct GnomeCmdStringDialog::Private
 {
     GnomeCmdStringDialogCallback ok_cb;
     GFunc cancel_cb;
@@ -110,7 +110,7 @@ static void class_init (GnomeCmdStringDialogClass *klass)
 
 static void init (GnomeCmdStringDialog *string_dialog)
 {
-    string_dialog->priv = g_new0 (GnomeCmdStringDialogPrivate, 1);
+    string_dialog->priv = g_new0 (GnomeCmdStringDialog::Private, 1);
     string_dialog->rows = -1;
 }
 

libgcmd/gnome-cmd-string-dialog.h
@@ -29,15 +29,17 @@
 #define GNOME_CMD_STRING_DIALOG_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS((obj), GNOME_CMD_TYPE_STRING_DIALOG, GnomeCmdStringDialogClass))
 
 
-xstruct GnomeCmdStringDialog
+struct GnomeCmdStringDialog
 {
     GnomeCmdDialog parent;
+
+    struct Private;
+
+    Private *priv;
 
     gint rows;
     GtkWidget **labels;
     GtkWidget **entries;
-
-    GnomeCmdStringDialogPrivate *priv;
 };
 
 