66681b1c5ce262593406fd80f747a1e35470f174
Piotr Eljasiak <epiotr@src.gnome.org>
2011-11-11 15:00:50 +0100

    noop: code cleanup

--- src/gnome-cmd-data.h
+++ src/gnome-cmd-data.h
@@ -353,7 +353,7 @@ struct GnomeCmdData
     void load_more();
     void save();
 
-    gboolean hide_type(GnomeVFSFileType type) const {  return filter_settings.file_types[type];  }
+    gboolean hide_type(GnomeVFSFileType type) const    {  return options.filter.file_types[type];   }
     GnomeCmdConRemote *get_quick_connect() const       {  return quick_connect;                     }
 
     GnomeCmdFileList::ColumnID get_sort_col(FileSelectorID id) const;
