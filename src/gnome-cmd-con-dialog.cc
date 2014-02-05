91de69692b637a3349319275e685da8e0ae6ef01
Daniel Mustieles <daniel.mustieles@gmail.com>
2014-02-05 21:07:13 +0100

    Partially updated FSF's address
    Signed-off-by: Uwe Scholz <uwescholz@src.gnome.org>

--- src/gnome-cmd-con-dialog.cc
+++ src/gnome-cmd-con-dialog.cc
@@ -15,36 +15,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
-
-/* Baobab - (C) 2005 Fabio Marzocca
-
-    baobab-remote-connect-dialog.c
-
-   Modified module from nautilus-connect-server-dialog.c
-   Released under same licence
- */
-/*
- * Nautilus
- *
- * Copyright (C) 2003 Red Hat, Inc.
- *
- * Nautilus is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * Nautilus is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program; see the file COPYING.  If not,
- * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+    along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <config.h>
