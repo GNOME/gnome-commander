6f82fb179089e7bb7108a5f50b8f2ca58aae0ed0
Uwe Scholz <uwescholz@src.gnome.org>
2015-10-12 21:53:14 +0200

    Adds a first simple unit test for googletest framework

--- tests/iv_fileops.cc
+++ tests/iv_fileops.cc
@@ -17,62 +17,35 @@
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
-
-   Author: Assaf Gordon  <agordon88@gmail.com>
 */
 
-#include <glib.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <libgviewer/libgviewer.h>
-#include <libgviewer/gvtypes.h>
-#include <libgviewer/fileops.h>
+#include "gtest/gtest.h"
+#include <iv_fileops.h>
 
-void usage()
-{
-    fprintf(stderr,"This program tests the file operations module in 'libgviewer'.\n\n");
+FileOpsTest::FileOpsTest() {}
+FileOpsTest::~FileOpsTest() {}
 
-    fprintf(stderr,"Usage: test-fileops filename [start_offset] [end_offset]\n");
-    fprintf(stderr,"\t'filename' will be printed to STDOUT.\n");
-    fprintf(stderr,"\t'start_offset' and 'end_offset' are optimal.\n\n");
-    exit(0);
-}
-
-int main(int argc, char *argv[])
-{
+TEST_F(FileOpsTest, gv_file_get_byte_does_read) {
+    const char *file_path = "../INSTALL";
     ViewerFileOps *fops;
-    offset_type start = 0;
-    offset_type end = 0;
+    offset_type end;
     offset_type current;
     int value;
 
-    if (argc<=1)
-        usage();
-
     fops = gv_fileops_new();
 
-    if (gv_file_open(fops, argv[1])==-1) {
-        fprintf(stderr,"Failed to open \"%s\"\n", argv[1]);
-        goto error;
-    }
+    ASSERT_NE (-1, gv_file_open(fops, file_path));
 
-    start = argc>=3 ? atol(argv[2]) : 0;
+    end = gv_file_get_max_offset(fops);
 
-    end = argc>=4 ? atol(argv[3]) : gv_file_get_max_offset(fops);
-
-
-    for (current = start; current < end; current++)
+    for (current = 0; current < end; current++)
     {
         value = gv_file_get_byte(fops, current);
-
-        if (value==-1)
-            break;
-
-        printf("%c",(unsigned char)value);
+        ASSERT_TRUE (0 <= value && value <= 255 );
     }
 
-error:
+    ASSERT_NE(-1, gv_file_open(fops, file_path));
+
     gv_file_free(fops);
     g_free(fops);
-    return 0;
 }
