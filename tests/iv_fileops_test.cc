b6679457f316e0d191d44ede907c2c12e4fdb971
Uwe Scholz <uwescholz@src.gnome.org>
2015-10-18 20:33:59 +0200

    Source files of tests will end on _test from now on

--- tests/iv_fileops_test.cc
+++ tests/iv_fileops_test.cc
@@ -2,7 +2,7 @@
   GNOME Commander - A GNOME based file manager
   Copyright (C) 2001-2006 Marcus Bjurman
   Copyright (C) 2007-2012 Piotr Eljasiak
-    Copyright (C) 2013-2015 Uwe Scholz
+  Copyright (C) 2013-2015 Uwe Scholz
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 */
 
 #include "gtest/gtest.h"
-#include <iv_fileops.h>
+#include <iv_fileops_test.h>
 
 FileOpsTest::FileOpsTest() {}
 FileOpsTest::~FileOpsTest() {}
