cea570ef06c3f3d57554d2a954c13b837198d406
Uwe Scholz <u.scholz83@gmx.de>
2018-03-04 13:15:23 +0100

    Adds output of commands in some scripts, fixes handling of filenames with spaces

--- gcmd-scripts/duplicate.sh
+++ gcmd-scripts/duplicate.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #name: Duplicate
+#term: true
 
 # Written by puux <puuxmine@gmail.com> 2016
 # Part of gnome-commander script plug-in system
 
-for n in $@
+for n in "$@"
 do
-  cp $n "1_$n"
+  cp "$n" "1_$n"
+  echo "cp \"$n\" \"1_$n\""
 done
 
