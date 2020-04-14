2aad9b4d3bc6af854797d7323ba38c48cecf462b
Uwe Scholz <u.scholz83@gmx.de>
2020-04-14 21:06:29 +0200

    Copyright update

--- tests/utils_no_dependencies_tests.cc
+++ tests/utils_no_dependencies_tests.cc
@@ -7,7 +7,7 @@
  * GCMD. Currently, only a single test is implemented in which a short
  * pattern of integers is searched inside a bigger array of integers.
  *
- * @copyright (C) 2013-2019 Uwe Scholz\n
+ * @copyright (C) 2013-2020 Uwe Scholz\n
  *
  * @copyright This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
