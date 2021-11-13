02abdb4e2b8c92a6ab868b422bfaf7a7e1fad018
Uwe Scholz <u.scholz83@gmx.de>
2021-11-13 22:09:47 +0100

    Free unused memory after creating a parent GnomeCmdDir

--- src/gnome-cmd-dir.cc
+++ src/gnome-cmd-dir.cc
@@ -380,7 +380,11 @@ GnomeCmdDir *gnome_cmd_dir_get_parent (GnomeCmdDir *dir)
 
     GnomeCmdPath *path = dir->priv->path->get_parent();
 
-    return path ? gnome_cmd_dir_new (dir->priv->con, path) : nullptr;
+    auto gnomeCmdDir = path ? gnome_cmd_dir_new (dir->priv->con, path) : nullptr;
+
+    delete path;
+
+    return gnomeCmdDir;
 }
 
 
