bdbf90901a9d210c571de9f4cac8cabb2439240d
Uwe Scholz <u.scholz83@gmx.de>
2023-01-21 20:31:40 +0100

    Copyright update

--- src/utils-no-dependencies.h
+++ src/utils-no-dependencies.h
@@ -1,7 +1,7 @@
 /** 
  * @file utils_no_dependencies.h
  * 
- * @copyright (C) 2013-2022 Uwe Scholz\n
+ * @copyright (C) 2013-2023 Uwe Scholz\n
  *
  * @copyright This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
