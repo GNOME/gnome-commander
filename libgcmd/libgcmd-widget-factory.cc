a1b781d8370ae0b7a5f6ea35b7184756bea10dc2
Andrey Kutejko <andy128k@gmail.com>
2024-12-28 15:22:57 +0100

    Remove dead code

--- libgcmd/libgcmd-widget-factory.cc
+++ libgcmd/libgcmd-widget-factory.cc
@@ -222,19 +222,6 @@ GtkWidget *create_radio (GtkWidget *parent, GtkWidget *group, const gchar *text,
 }
 
 
-GtkWidget *create_radio_with_mnemonic (GtkWidget *parent, GtkWidget *group, gchar *text, const gchar *name)
-{
-    GtkWidget *radio = gtk_check_button_new_with_label (text);
-    gtk_check_button_set_group (GTK_CHECK_BUTTON (radio), GTK_CHECK_BUTTON (group));
-
-    g_object_ref (radio);
-    g_object_set_data_full (G_OBJECT (parent), name, radio, g_object_unref);
-    gtk_widget_show (radio);
-
-    return radio;
-}
-
-
 GtkWidget *create_spin (GtkWidget *parent, const gchar *name, gint min, gint max, gint value)
 {
     GtkAdjustment *adj = GTK_ADJUSTMENT (gtk_adjustment_new (value, min, max, 1, 10, 0));
@@ -247,99 +234,6 @@ GtkWidget *create_spin (GtkWidget *parent, const gchar *name, gint min, gint max
 }
 
 
-GtkWidget *create_color_button (GtkWidget *parent, const gchar *name)
-{
-    GtkWidget *w = gtk_color_button_new ();
-    g_object_ref (w);
-    g_object_set_data_full (G_OBJECT (parent), name, w, g_object_unref);
-    gtk_widget_show (w);
-    return w;
-}
-
-
-static void icon_button_set_path (GtkButton *button, const gchar* icon_path)
-{
-    g_object_set_data_full (G_OBJECT (button), "file", g_strdup (icon_path), g_free);
-
-    if (icon_path && *icon_path != '\0')
-    {
-        auto image = gtk_image_new ();
-        gtk_image_set_from_file (GTK_IMAGE (image), icon_path);
-        gtk_widget_set_tooltip_text (GTK_WIDGET (button), icon_path);
-        gtk_button_set_child (GTK_BUTTON (button), GTK_WIDGET (image));
-    }
-    else
-    {
-        gtk_button_set_child (GTK_BUTTON (button), gtk_label_new (_("Choose Icon")));
-    }
-}
-
-
-static void icon_button_chooser_response (GtkDialog *dialog, int response_id, gpointer user_data)
-{
-    GtkButton *button = GTK_BUTTON (user_data);
-
-    if (response_id == GTK_RESPONSE_ACCEPT)
-    {
-        auto icon_file = gtk_file_chooser_get_file (GTK_FILE_CHOOSER (dialog));
-        auto icon_path = g_file_get_path (icon_file);
-        icon_button_set_path (button, icon_path);
-        g_free (icon_path);
-        g_object_unref (icon_file);
-    }
-
-    gtk_window_destroy (GTK_WINDOW (dialog));
-}
-
-
-static void icon_button_clicked (GtkButton *button, const gchar* iconPath)
-{
-    GtkWidget *dialog;
-    GtkFileFilter *filter;
-
-    dialog = gtk_file_chooser_dialog_new (_("Select an Image File"),
-                                            GTK_WINDOW (gtk_widget_get_ancestor ((GtkWidget*)button, GTK_TYPE_WINDOW)),
-                                            GTK_FILE_CHOOSER_ACTION_OPEN,
-                                            _("_Cancel"), GTK_RESPONSE_CANCEL,
-                                            _("_OK"), GTK_RESPONSE_ACCEPT,
-                                            nullptr);
-    if (iconPath)
-    {
-        auto folderPath = g_path_get_dirname(iconPath);
-        GFile *folder = g_file_new_for_path (folderPath);
-        gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), folder, nullptr);
-        g_object_unref (folder);
-        g_free(folderPath);
-    }
-    else
-    {
-        GFile *folder = g_file_new_for_path (PIXMAPS_DIR);
-        gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), folder, nullptr);
-        g_object_unref (folder);
-    }
-
-    filter = gtk_file_filter_new ();
-    gtk_file_filter_add_pixbuf_formats (filter);
-    gtk_file_chooser_set_filter (GTK_FILE_CHOOSER (dialog), filter);
-
-    g_signal_connect (dialog, "response", G_CALLBACK (icon_button_chooser_response), button);
-    gtk_window_present (GTK_WINDOW (dialog));
-}
-
-
-GtkWidget *create_icon_button_widget (GtkWidget *parent, const gchar *name, const gchar *iconPath)
-{
-    auto gtkButton = gtk_button_new ();
-    icon_button_set_path (GTK_BUTTON (gtkButton), iconPath);
-    g_signal_connect (gtkButton, "clicked", G_CALLBACK (icon_button_clicked), (gpointer) iconPath);
-    g_object_ref (gtkButton);
-    g_object_set_data_full (G_OBJECT (parent), name, gtkButton, g_object_unref);
-    gtk_widget_show (gtkButton);
-
-    return gtkButton;
-}
-
-
 GtkWidget *create_scale (GtkWidget *parent, const gchar *name, gint value, gint min, gint max)
 {
     GtkWidget *scale = gtk_scale_new (GTK_ORIENTATION_HORIZONTAL, GTK_ADJUSTMENT (gtk_adjustment_new (value, min, max, 0, 0, 0)));
@@ -349,59 +243,6 @@ GtkWidget *create_scale (GtkWidget *parent, const gchar *name, gint value, gint 
     gtk_scale_set_digits (GTK_SCALE (scale), 0);
 
     return scale;
-}
-
-
-static void directory_chooser_response (GtkNativeDialog *chooser, gint response_id, GtkWidget *button)
-{
-    if (response_id != GTK_RESPONSE_ACCEPT)
-        return;
-
-    GFile *file = gtk_file_chooser_get_file (GTK_FILE_CHOOSER (chooser));
-    if (file != nullptr)
-    {
-        directory_chooser_button_set_file (button, file);
-        g_object_unref (file);
-    }
-}
-
-
-static void on_directory_chooser_click (GtkButton *button, GtkWidget *parent)
-{
-    auto chooser = gtk_file_chooser_native_new (_("Select Directory"),
-        GTK_WINDOW (parent),
-        GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
-        nullptr,
-        nullptr);
-    gtk_native_dialog_set_modal (GTK_NATIVE_DIALOG (chooser), true);
-    g_signal_connect (chooser, "response", G_CALLBACK (directory_chooser_response), button);
-
-    if (GFile *file = directory_chooser_button_get_file (GTK_WIDGET (button)))
-        gtk_file_chooser_set_file (GTK_FILE_CHOOSER (chooser), file, nullptr);
-
-    gtk_native_dialog_show (GTK_NATIVE_DIALOG (chooser));
-}
-
-
-GtkWidget *create_directory_chooser_button (GtkWidget *parent, const gchar *name)
-{
-    GtkWidget *button = create_named_button (parent, _("(None)"), name, G_CALLBACK (on_directory_chooser_click));
-    return button;
-}
-
-
-GFile *directory_chooser_button_get_file (GtkWidget *button)
-{
-    return (GFile *) g_object_get_data (G_OBJECT (button), "file");
-}
-
-
-void directory_chooser_button_set_file (GtkWidget *button, GFile *file)
-{
-    g_object_set_data_full (G_OBJECT (button), "file", g_object_ref (file), g_object_unref);
-    gchar *label = file ? g_file_get_basename (file) : _("(None)");
-    gtk_button_set_label (GTK_BUTTON (button), label);
-    g_free (label);
 }
 
 
@@ -480,15 +321,6 @@ void create_treeview_column (GtkWidget *sw, gint col, gint width, const gchar *l
 }
 
 
-GtkWidget *create_combo_box_text_with_entry (GtkWidget *parent)
-{
-    GtkWidget *combo = gtk_combo_box_text_new_with_entry ();
-    g_object_ref (combo);
-    g_object_set_data_full (G_OBJECT (parent), "combo", combo, g_object_unref);
-    gtk_widget_show (combo);
-    return combo;
-}
-
 GtkWidget *create_combo_box_text (GtkWidget *parent, const gchar **items)
 {
     GtkWidget *combo = gtk_combo_box_text_new ();
@@ -512,27 +344,6 @@ GtkWidget *create_progress_bar (GtkWidget *parent)
     gtk_widget_show (w);
     gtk_progress_bar_set_show_text (GTK_PROGRESS_BAR (w), TRUE);
     return w;
-}
-
-
-GtkWidget *create_sw (GtkWidget *parent)
-{
-    GtkWidget *scrolledwindow = gtk_scrolled_window_new ();
-    g_object_ref (scrolledwindow);
-    g_object_set_data_full (G_OBJECT (parent), "scrolledwindow", scrolledwindow, g_object_unref);
-    gtk_widget_show (scrolledwindow);
-    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-    return scrolledwindow;
-}
-
-
-const char *get_entry_text (GtkWidget *parent, const gchar *entry_name)
-{
-    GtkWidget *entry = lookup_widget (parent, entry_name);
-    if (!entry) return nullptr;
-    if (!GTK_IS_ENTRY (entry)) return nullptr;
-
-    return gtk_editable_get_text (GTK_EDITABLE (entry));
 }
 
 
