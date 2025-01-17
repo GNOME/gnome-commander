55a312b6b5c9632c9ec2c2fb136c5aaede9297d5
Andrey Kutejko <andy128k@gmail.com>
2025-01-17 13:13:38 +0100

    Update POTFILES.in and add a script for its generation

---
+++ git-scripts/update-potfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat << EOF > po/POTFILES.in
+# List of source files containing translatable strings.
+# Please keep this file sorted alphabetically.
+data/org.gnome.gnome-commander.appdata.xml.in
+data/org.gnome.gnome-commander.desktop.in
+data/org.gnome.gnome-commander.gschema.xml
+EOF
+
+{
+  grep -RHo 'gettextrs' src/ | cut -d: -f1 && \
+  find src/ plugins/ -regex '.*\.\(cc\|\c\)$' -exec grep -lE "\bN?_\s*\(" '{}' \;
+} | sort -u >> po/POTFILES.in
