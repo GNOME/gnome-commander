bddf3d006816887fd70cb6770a6d6a389e397f1e
Uwe Scholz <u.scholz83@gmx.de>
2026-03-18 21:57:26 +0100

    Update the GPL version mentioned in the program to v3.0+; see commit 0c9e32a1

--- src/user_actions.rs
+++ src/user_actions.rs
@@ -1172,13 +1172,13 @@ Copyright \u{00A9} 2026 Wladimir Palant";
     let license = format!(
         "{}\n\n{}\n\n{}",
         gettext(
-            "GNOME Commander is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version."
+            "GNOME Commander is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version."
         ),
         gettext(
             "GNOME Commander is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details."
         ),
         gettext(
-            "You should have received a copy of the GNU General Public License along with GNOME Commander; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA."
+            "You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>."
         )
     );
 
