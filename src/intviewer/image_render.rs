15829edfcc66cb92556b07e4ff64444d927aa0e1
Wladimir Palant <167073-palant@users.noreply.gitlab.gnome.org>
2026-04-15 14:19:13 +0200

    Updated file’s copyright note

--- src/intviewer/image_render.rs
+++ src/intviewer/image_render.rs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2025 Andrey Kutejko <andy128k@gmail.com>
+// SPDX-FileCopyrightText: 2026 Wladimir Palant https://palant.info/
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
