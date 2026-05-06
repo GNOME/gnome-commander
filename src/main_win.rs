59a4ec1fd32b6953732a6f112464d961ab2b243c
Wladimir Palant <167073-palant@users.noreply.gitlab.gnome.org>
2026-05-06 02:05:49 +0200

    Select correct panel side on startup

--- src/main_win.rs
+++ src/main_win.rs
@@ -10,7 +10,7 @@ use crate::{
     },
     dir::Directory,
     file::{File, FileOps},
-    file_selector::{FileSelector, TabPosition, TabVariant},
+    file_selector::{FileSelector, TabPosition, TabState, TabVariant},
     layout::color_themes::ColorThemes,
     libgcmd::{
         file_actions::{FileActions, FileActionsExt},
@@ -1054,11 +1054,16 @@ impl MainWindow {
             right_tabs.push(TabVariant::new(Default::default(), dir.to_string()));
         }
 
+        let current_panel = if right_tabs.iter().any(|tab| tab.state() == TabState::Active) {
+            1
+        } else {
+            0
+        };
         self.file_selector(FileSelectorID::Left)
             .open_tabs(left_tabs);
-
         self.file_selector(FileSelectorID::Right)
             .open_tabs(right_tabs);
+        self.set_current_panel(current_panel);
     }
 
     fn save_tabs(&self, save_all: bool, save_current: bool, save_history: bool) -> WriteResult {
