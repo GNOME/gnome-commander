8c8eeb32b56b40413773beb59c6b442e8e2512e2
Wladimir Palant <167073-palant@users.noreply.gitlab.gnome.org>
2026-05-29 23:53:45 +0200

    Merge branch 'quick-filter-tab-switching' into 'main'
    Fixes #363 - Make sure to reset Quick Filter when switching tabs
    
    Closes #363
    
    See merge request GNOME/gnome-commander!540
    
    (cherry picked from commit f1f069a23402f4fc25dfefd62301247002c23b60)
    
    f319e8c0 Fixes #363 - Make sure to reset Quick Filter when switching tabs
    
    Co-authored-by: Wladimir Palant <167073-palant@users.noreply.gitlab.gnome.org>

--- src/file_list/quick_search.rs
+++ src/file_list/quick_search.rs
@@ -156,7 +156,12 @@ mod imp {
         }
     }
 
-    impl WidgetImpl for QuickSearch {}
+    impl WidgetImpl for QuickSearch {
+        fn unrealize(&self) {
+            self.parent_unrealize();
+            self.obj().file_list().set_filter(None);
+        }
+    }
 
     impl QuickSearch {
         /// Moves focus to the previously focused widget, making sure that entry field state isn't
@@ -408,7 +413,6 @@ impl QuickSearch {
 
     pub fn remove(&self) {
         self.unparent();
-        self.file_list().set_filter(None);
         self.file_list().grab_focus();
     }
 
