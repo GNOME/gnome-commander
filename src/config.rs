7c04beb71f6fb61db7fc17877b7ec845cb595c2b
Wladimir Palant <167073-palant@users.noreply.gitlab.gnome.org>
2026-06-01 17:49:51 +0200

    Fixes #367 - Allow generating portable builds

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,10 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-use std::{fs, path::PathBuf};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
 
 pub const PACKAGE: &str = env!("CARGO_PKG_NAME");
 pub const ICONS_PREFIX: &str = "/org/gnome/gnome-commander/icons";
@@ -28,25 +31,45 @@ const LOCAL_PLUGIN_DIR: Option<&str> = option_env!("LOCAL_PLUGIN_DIR");
 #[cfg(not(debug_assertions))]
 const LOCAL_PLUGIN_DIR: Option<&str> = None;
 
+const GLOBAL_SCHEMA_DIR: Option<&str> = option_env!("GLOBAL_SCHEMA_DIR");
 #[cfg(debug_assertions)]
 const LOCAL_SCHEMA_DIR: Option<&str> = option_env!("LOCAL_SCHEMA_DIR");
 #[cfg(not(debug_assertions))]
 const LOCAL_SCHEMA_DIR: Option<&str> = None;
 
+const SETTINGS_KEYFILE: Option<&str> = option_env!("SETTINGS_KEYFILE");
+
+fn resolve_path(path: &str) -> PathBuf {
+    env::current_exe()
+        .ok()
+        .and_then(|executable| executable.parent().map(Path::to_owned))
+        .map(|dir| dir.join(path))
+        .unwrap_or_else(|| PathBuf::from(path))
+}
+
 pub fn locale_dir() -> PathBuf {
-    LOCAL_LOCALE_DIR
-        .filter(|dir| fs::exists(dir).is_ok_and(|result| result))
-        .unwrap_or(GLOBAL_LOCALE_DIR)
-        .into()
+    resolve_path(
+        LOCAL_LOCALE_DIR
+            .filter(|dir| fs::exists(dir).is_ok_and(|result| result))
+            .unwrap_or(GLOBAL_LOCALE_DIR),
+    )
 }
 
 pub fn plugin_dir() -> PathBuf {
-    LOCAL_PLUGIN_DIR
-        .filter(|dir| fs::exists(dir).is_ok_and(|result| result))
-        .unwrap_or(GLOBAL_PLUGIN_DIR)
-        .into()
+    resolve_path(
+        LOCAL_PLUGIN_DIR
+            .filter(|dir| fs::exists(dir).is_ok_and(|result| result))
+            .unwrap_or(GLOBAL_PLUGIN_DIR),
+    )
 }
 
 pub fn schema_dir() -> Option<PathBuf> {
-    LOCAL_SCHEMA_DIR.map(PathBuf::from)
+    LOCAL_SCHEMA_DIR
+        .filter(|dir| fs::exists(dir).is_ok_and(|result| result))
+        .or(GLOBAL_SCHEMA_DIR)
+        .map(resolve_path)
+}
+
+pub fn settings_keyfile() -> Option<PathBuf> {
+    SETTINGS_KEYFILE.map(resolve_path)
 }
