dde4fab1b9b335fd272aeac39c3ec3e8d64219e7
Wladimir Palant <167073-palant@users.noreply.gitlab.gnome.org>
2026-06-02 13:48:55 +0200

    Moved ContainerExt trait into a separate module

--- gcmd-component-framework/src/lib.rs
+++ gcmd-component-framework/src/lib.rs
@@ -1,57 +1,13 @@
-// SPDX-FileCopyrightText: 2025 Andrey Kutejko <andy128k@gmail.com>
 // SPDX-FileCopyrightText: 2026 Wladimir Palant https://palant.info/
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+pub mod container;
 
 use futures::{
     FutureExt,
     future::{pending, select_all},
 };
-use gtk::prelude::*;
-
-/// A helper trait used by the [with! macro](crate::with), provides a standard API to add a child to
-/// some Gtk widgets. This trait is used when no explicit method is specified in `with!`.
-pub trait ContainerExt<T> {
-    fn container_set_child(&self, child: &T);
-}
-
-macro_rules! container_ext_impl {
-    ($container_type:ty, $method:ident, $child_type:ty) => {
-        impl<T: IsA<$child_type>> ContainerExt<T> for $container_type {
-            fn container_set_child(&self, child: &T) {
-                self.$method(child.as_ref().into());
-            }
-        }
-
-        impl<T: IsA<$child_type>> ContainerExt<T> for &$container_type {
-            fn container_set_child(&self, child: &T) {
-                self.$method(child.as_ref().into());
-            }
-        }
-    };
-}
-
-container_ext_impl!(gtk::ApplicationWindow, set_child, gtk::Widget);
-container_ext_impl!(gtk::Box, append, gtk::Widget);
-container_ext_impl!(gtk::Button, set_child, gtk::Widget);
-container_ext_impl!(gtk::CheckButton, set_child, gtk::Widget);
-container_ext_impl!(gtk::Expander, set_child, gtk::Widget);
-container_ext_impl!(gtk::Frame, set_child, gtk::Widget);
-container_ext_impl!(gtk::LinkButton, set_child, gtk::Widget);
-container_ext_impl!(gtk::ListBox, append, gtk::Widget);
-container_ext_impl!(gtk::ListBoxRow, set_child, gtk::Widget);
-container_ext_impl!(gtk::MenuButton, set_child, gtk::Widget);
-container_ext_impl!(gtk::Popover, set_child, gtk::Widget);
-container_ext_impl!(gtk::Revealer, set_child, gtk::Widget);
-container_ext_impl!(gtk::ScrolledWindow, set_child, gtk::Widget);
-container_ext_impl!(gtk::SearchBar, set_child, gtk::Widget);
-container_ext_impl!(gtk::Stack, add_child, gtk::Widget);
-container_ext_impl!(gtk::StackSidebar, set_stack, gtk::Stack);
-container_ext_impl!(gtk::StackSwitcher, set_stack, gtk::Stack);
-container_ext_impl!(gtk::ToggleButton, set_child, gtk::Widget);
-container_ext_impl!(gtk::TreeExpander, set_child, gtk::Widget);
-container_ext_impl!(gtk::Window, set_child, gtk::Widget);
-container_ext_impl!(gtk::WindowHandle, set_child, gtk::Widget);
 
 /// A helper macro to compose a view.
 ///
@@ -119,9 +75,9 @@ container_ext_impl!(gtk::WindowHandle, set_child, gtk::Widget);
 /// assert_eq!(view.window.child(), view.button.parent());
 /// ```
 ///
-/// By default, child widgets will be added using [ContainerExt trait](ContainerExt) which is
-/// implemented for various Gtk widget types. Some container types have multiple types to add
-/// children however. In such cases the method can be called explicitly:
+/// By default, child widgets will be added using [ContainerExt trait](container::ContainerExt)
+/// which is implemented for various Gtk widget types. Some container types have multiple types to
+/// add children however. In such cases the method can be called explicitly:
 ///
 /// ```rust
 /// # use gcmd_component_framework::with;
@@ -211,7 +167,7 @@ macro_rules! with {
         }
         $($rest:tt)*
     }, $obj:ident) => {
-        $crate::ContainerExt::container_set_child(
+        $crate::container::ContainerExt::container_set_child(
             &$obj, &$crate::with!($type => {$($ops)*})
         );
         $crate::with!{@block {$($rest)*}, $obj}
@@ -223,7 +179,7 @@ macro_rules! with {
         }
         $($rest:tt)*
     }, $obj:ident) => {
-        $crate::ContainerExt::container_set_child(
+        $crate::container::ContainerExt::container_set_child(
             &$obj, $crate::with!($expr => {$($ops)*})
         );
         $crate::with!{@block {$($rest)*}, $obj}
